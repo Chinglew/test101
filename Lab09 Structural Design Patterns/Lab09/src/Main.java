8171b698176fda8a513394e3a8a9db4775d6ff1f
Chinglew <47999198+Chinglew@users.noreply.github.com>
2022-03-30 16:01:40 +0700

    fix lab09

--- Lab09 Structural Design Patterns/Lab09/src/Main.java
+++ Lab09 Structural Design Patterns/Lab09/src/Main.java
@@ -43,9 +43,23 @@ public class Main {
         System.out.println("The total price of the Good Doctor Team is " + theGoodDoctor.getPrice());
 
         System.out.println("================================");
+        
+        HealthcareWorkerTeam KMITL_Doctor = new HealthcareWorkerTeam();
+        Surgeon Pasin = new Surgeon("Pasin C", 1500);
+        Surgeon Paritat = new Surgeon("Paritat t", 3000);
+        KMITL_Doctor.addMember(Pasin);
+        KMITL_Doctor.addMember(Paritat);
+
+        System.out.println("================================");
+        theGoodDoctor.service();
+        System.out.println("The total price of KMITL Doctor Team is " + KMITL_Doctor.getPrice());
+
+        System.out.println("================================");
+        
         HealthcareWorkerTeam healthcareSeries = new HealthcareWorkerTeam();
         healthcareSeries.addMember(teamMedicalDragon);
         healthcareSeries.addMember(theGoodDoctor);
+        healthcareSeries.addMember(KMITL_Doctor);
         System.out.println("The total price of the healthcare series is " + healthcareSeries.getPrice());
     }
 }
