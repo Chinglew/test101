fc78c0165e124174b772b1f12bf7c8ec93fa19c6
Chinglew <47999198+Chinglew@users.noreply.github.com>
2022-04-06 21:34:54 +0700

    add lil subclass but still not sure

--- Lab10_Behavioral Design Pattern/Lab10/src/NewsReader.java
+++ Lab10_Behavioral Design Pattern/Lab10/src/NewsReader.java
@@ -5,13 +5,18 @@
 public class NewsReader extends NewsSubscriber{
     
     private String r_name;
+    private NewsSubscriber newsSubscriber;
+    
     public NewsReader(String name) {
         this.r_name = name;
+        newsSubscriber = new NewsSubscriber();
     }
-    
+
     public void update(String m) {
         System.out.println(this.r_name+" got news :: " + m);
     }
+    
+    
     
     
     
